d584ada240d91d6f4f19a44c3eaca2e786e80a1a
Mikhail Pospelov <mixan946@yandex.ru>
2014-02-13 15:58:40 +0400

    add changes

--- add_user.rb
+++ add_user.rb
@@ -1,11 +1,18 @@
 require 'byebug'
-dep 'add_user', :username, :password do
-  username.ask("Type 'username' which you want to add")
+dep 'add_user', :username, :app_path, :password do
+
+  username.ask("Type 'username' which you want to add?")
+  app_path.ask("Tyle 'app_path' where you want to add permission for #{username}").default("/rails_projects/#{username}")
+  password.ask("Type 'password' which you want to add?")
+
   met? do
     debugger
-    "cat /etc/passwd | cut -d: -f1".p.grep(username)
+    not shell? "cat /etc/passwd | cut -d: -f1 | grep #{username}" &&
+      app_path.p.exist?
   end
+
   meet do
-    "useradd -m -s /bin/bash -p PASSWORD USER_NAME"
+    shell "adduser --no-create-home --home #{app_path} -p #{password} #{username}"
   end
+
 end
