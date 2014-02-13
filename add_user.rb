6b06f57f5d5e9af1a9d3bcdfc42a2b416edff30a
Mikhail Pospelov <mixan946@yandex.ru>
2014-02-13 16:37:40 +0400

    change password

--- add_user.rb
+++ add_user.rb
@@ -3,6 +3,7 @@ dep 'add_user', :username, :app_path do
 
   username.ask("Type 'username' which you want to add?")
   app_path.ask("Tyle 'app_path' where you want to add permission for #{username}").default("/rails_projects/#{username}")
+  password.ask("Type 'password' which you want to add?")
 
   met? do
     shell? "cat /etc/passwd | cut -d: -f1 | grep #{username}"
@@ -10,7 +11,8 @@ dep 'add_user', :username, :app_path do
 
   meet do
     if app_path.p.exist?
-      shell "adduser --no-create-home --home #{app_path} --ingroup sshusers dr_tai"
+      shell "adduser --no-create-home --home #{app_path} --ingroup sshusers #{username}"
+      shell "echo dr_tai:#{password} | chpasswd"
     else
       raise "There no directory #{app_path}"
     end
