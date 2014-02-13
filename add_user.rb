29ddf7b131479557b383647d84bfda2162586139
Mikhail Pospelov <mixan946@yandex.ru>
2014-02-13 15:30:12 +0400

    add users

---
+++ add_user.rb
@@ -0,0 +1,10 @@
+dep 'add_user', :username, :password do
+  username.ask("Type 'username' which you want to add")
+  met? do
+    debugger
+    "cat /etc/passwd | cut -d: -f1".p.grep(username)
+  end
+  meet do
+    "useradd -m -s /bin/bash -p PASSWORD USER_NAME"
+  end
+end
