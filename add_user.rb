35d1a7865b1cb5f33620e63fe86bde0fb07ebc77
Mikhail Pospelov <mixan946@yandex.ru>
2014-02-13 16:33:30 +0400

    adding to group

--- add_user.rb
+++ add_user.rb
@@ -1,9 +1,8 @@
 require 'byebug'
-dep 'add_user', :username, :app_path, :password do
+dep 'add_user', :username, :app_path do
 
   username.ask("Type 'username' which you want to add?")
   app_path.ask("Tyle 'app_path' where you want to add permission for #{username}").default("/rails_projects/#{username}")
-  password.ask("Type 'password' which you want to add?")
 
   met? do
     shell? "cat /etc/passwd | cut -d: -f1 | grep #{username}"
@@ -11,8 +10,7 @@ dep 'add_user', :username, :app_path, :password do
 
   meet do
     if app_path.p.exist?
-      debugger
-      shell "adduser --no-create-home --home #{app_path} -p #{password} #{username} sshusers"
+      shell "adduser --no-create-home --home #{app_path} --ingroup sshusers dr_tai"
     else
       raise "There no directory #{app_path}"
     end
