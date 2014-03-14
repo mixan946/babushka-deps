98d422f1ef4fff36e258160bb44f1244300af0c3
Mikhail Pospelov <mixan946@yandex.ru>
2014-03-14 18:59:38 +0400

    add helpers

---
+++ helpers/rvm.rb
@@ -0,0 +1,20 @@
+def rvm_script
+    "rvm" #/usr/local/rvm/scripts/rvm"
+end
+
+def rvm_installed?
+  "/usr/local/rvm".p.exists?
+end
+
+def rvm_run cmd
+    log_shell("rvm_run: #{cmd}", rvm_script + " " + cmd)
+end
+
+def rvm_run_with_ruby ruby_version, cmd, &block
+    log_shell("rvm_run: #{cmd}", rvm_script + " use #{ruby_version} do " + cmd, &block)
+end
+
+def current_rubies
+  out = rvm_run("list rubies")
+  rubies = out.scan(/^[=> ]{3}([^ ]+) /).flatten
+end
